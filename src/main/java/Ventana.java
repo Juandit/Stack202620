dcff955ddc3b8b0e16fb08926e394a5e7a74f19d
Laboratorio <laboratorio@udla-ec.int>
2026-04-13 15:36:08 -0500

    Sexta Pila

---
+++ src/main/java/Ventana.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Ventana {
+    private JTextField TEXTO;
+    private JButton btnEditar;
+    private JTextArea textArea1;
+    private JButton verUltimoButton;
+    private JButton btnDeshacer;
+}
